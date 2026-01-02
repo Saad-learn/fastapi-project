2dd50c37408c7f0a4503da612d340def6f0f1c1e
saad <kingsaads705@gmail.com>
2026-01-02 19:18:13 +0500

    first commit

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -3,9 +3,6 @@ from fastapi.testclient import TestClient
 from app.main import app
 
 def client():
-    def fake_verify_token(token: str):
-        return{"sub": "admin", "role": "admin"}
-    app.dependency_overrides[verify_token] = fake_verify_token
     return TestClient(app)
 
 def admin_token():
@@ -17,10 +14,3 @@ def org_id():
 
 def user_id():
     return 1
-
-# conftest.py
-import warnings
-import pydantic._internal._config as _cfg
-
-def pytest_configure():
-    warnings.filterwarnings("ignore", category=_cfg.PydanticDeprecatedSince20)
